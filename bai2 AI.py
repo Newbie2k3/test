f5648905047f54a6c68362ea461583bed6515753
Newbie2k3 <thainguyenax@gmail.com>
2022-11-22 13:19:14 +0700

    Refesh

--- bai2 AI.py
+++
@@ -1,5 +0,0 @@
-for i in range(1,21):
-    listt = []
-    for j in range(1,i+1):
-        listt.append(j*j)
-    print(listt)    
--- bai6 AI.py
+++
@@ -1,7 +0,0 @@
-from math import ceil
-a = [int(x) for x in input().split()]
-b = [int(x) for x in input().split()]
-n = int(input())
-if (n >= ceil(sum(a)/5) + ceil(sum(b)/10) ):
-    print("YES")
-else : print("NO")
